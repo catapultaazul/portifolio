dc16bc2f7a28f7962ce3ccee72b8be4f7ea91b42
catapultaazul <lucasguilheme.31@gmail.com>
2021-12-28 11:52:55 -0300

    fixed a theme bug

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,19 +3,23 @@ import DarkModeToggle from "./DarkModeToggle";
 
 const Header = () => {
   const title = useColorModeValue("#810181", "purple.400");
+  const header = useColorModeValue("white", "gray.800");
+  const shadow = useColorModeValue(
+    "0px 0px 12px rgba(0,0,0,0.40)",
+    "0px 0px 12px #ffffff66"
+  );
 
   return (
     <>
       <Box
         display="flex"
-        bg="white"
+        bg={header}
         alignItems="center"
         justifyContent="space-between"
-        borderBottom="1px solid #e4e4e4"
         mb="2rem"
         px={["2rem", "2.5rem", "6rem", "8rem"]}
         py={["1.5rem", "2rem"]}
-        boxShadow="0px 0px 12px rgba(0,0,0,0.40)"
+        boxShadow={shadow}
         w="100vw"
         position="fixed"
         zIndex="1"
